cda4e109ec4afedbe4f4ec187bd5b2cd4323c903
Adam <adamcpenman@gmail.com>
2020-03-26 14:29:23 -0500

    import components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layouts/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+
+import ContactState from './context/contact/ContactState';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className='App'>
-      <h1>Hello World</h1>
-    </div>
+    <ContactState>
+      <Router>
+        <Fragment className='App'>
+          <Navbar />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </ContactState>
   );
-}
+};
 
 export default App;
