9cf26e7de1c358a8802e4d2e89ef8d6a9391c5a5
Adam <adamcpenman@gmail.com>
2020-03-29 12:20:55 -0500

    Login Auth

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -70,7 +70,31 @@ const AuthState = props => {
     }
   };
   // Login User
-  const login = () => console.log('login');
+  const login = async formData => {
+    console.log(register, 'register');
+    console.log(formData, 'formdata');
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+    console.log(config, 'confi');
+    try {
+      const res = await axios.post('/api/auth', formData, config);
+      console.log(res);
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      });
+
+      loadUser();
+    } catch (err) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.response.data.msg
+      });
+    }
+  };
 
   // Logout
   const logout = () => console.log('logout');
